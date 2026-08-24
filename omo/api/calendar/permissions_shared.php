23511be5692b9220e3d5007ce01b1a8786538927
DavidDrayer <david.draeyer@gmail.com>
2026-08-24 06:49:11 +0200

    Correct bug on permissions for MAJ events

--- omo/api/calendar/permissions_shared.php
+++ omo/api/calendar/permissions_shared.php
@@ -27,26 +27,13 @@ if (!function_exists('omoCalendarResolveEventPermissionHolon')) {
 if (!function_exists('omoCalendarCanUseEditEventPermission')) {
     function omoCalendarCanUseEditEventPermission(\dbObject\Holon $permissionHolon, int $organizationId, int $userId, bool $useSessionCache = true): bool
     {
-        static $configuredPermissionCache = [];
-
         $organizationId = (int)$organizationId;
         $userId = (int)$userId;
         if ($organizationId <= 0 || $userId <= 0) {
             return false;
         }
 
-        $cacheKey = $organizationId . ':' . $userId;
-        if (!array_key_exists($cacheKey, $configuredPermissionCache)) {
-            $permissionSet = \dbObject\HolonPermission::buildUserPermissionSetForOrganization(
-                $userId,
-                $organizationId,
-                ['CAN_EDIT_EVENT']
-            );
-            $configuredPermissionCache[$cacheKey] = !empty($permissionSet['definedPermissionKeys']['CAN_EDIT_EVENT']);
-        }
-
-        return $configuredPermissionCache[$cacheKey]
-            && $permissionHolon->isAllowed('CAN_EDIT_EVENT', $useSessionCache, $userId);
+        return $permissionHolon->isAllowed('CAN_EDIT_EVENT', $useSessionCache, $userId);
     }
 }
 
